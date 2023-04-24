78898e85cf1f77fed7a67a637cc6d2ae8185cd63
Leo Nava <89169005+LeoNava1@users.noreply.github.com>
2023-04-23 21:04:26 -0500

    Se añadio el query de la tabla user

--- conexion.php
+++ conexion.php
@@ -10,4 +10,19 @@
     if($conexion->error) {
         echo "Conexión con la base de datos.";
     }
+
+    //Queries necesarios para la base de datos
+
+    /*
+    CREATE TABLE user (
+        idUser int PRIMARY KEY AUTO_INCREMENT,
+        nameUser varchar(50) NOT NULL,
+        emailUser varchar(50) NOT NULL,
+        phoneUser varchar(15) NOT NULL,
+        guestNumUser int NOT NULL,
+        checkInDateUser datetime NOT NULL,
+        hours int NOT NULL,
+        roomTypeUser varchar(10) NOT NULL
+    );
+    */
 ?>
