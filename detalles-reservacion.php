e5b3dd7d8551bcb7bfed047113ce40f301035d0c
DavidMar29 <carlosdavidmarcruz@gmail.com>
2023-05-03 18:39:22 -0600

    Detalles reservacion front-end

--- detalles-reservacion.php
+++ detalles-reservacion.php
@@ -1,16 +1,17 @@
-<?php 
-    require_once "conexion.php";
+<?php
+require_once "conexion.php";
 
-    $idValue = $_GET['idUser'];
-        
-    $consultaDatos = "SELECT * FROM user WHERE idUser=$idValue;";
-    $reservacionesDetalles = $conexion->query($consultaDatos);
+$idValue = $_GET['idUser'];
 
-    $conexion->close();
+$consultaDatos = "SELECT * FROM user WHERE idUser=$idValue;";
+$reservacionesDetalles = $conexion->query($consultaDatos);
+
+$conexion->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,28 +21,62 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Roboto&display=swap" rel="stylesheet">
     <title>Registro de usuarios</title>
 </head>
+
 <body>
     <div class="container">
         <div class="main sombra">
-            <?php 
-                while ($reservacionEspecifica = $reservacionesDetalles->fetch_object()) {
-                    $CheckIn = $reservacionEspecifica->checkInDateUser;
-                    $hours = $reservacionEspecifica->hoursUser;
-                    $checkOut = date('Y-m-d H:i:s', strtotime($CheckIn . ' + ' . $hours . ' hours'));
-            ?>
-                    
-                <p><?php echo $reservacionEspecifica->nameUser;?><p>
-                <p><?php echo $reservacionEspecifica->emailUser;?><p>
-                <p><?php echo $reservacionEspecifica->phoneUser;?><p>
-                <p><?php echo $reservacionEspecifica->checkInDateUser;?><p>
-                <p><?php echo $checkOut;?><p>                
-                <p><?php echo $reservacionEspecifica->guestsNumUser;?><p>
-                <p><?php echo $reservacionEspecifica->roomTypeUser;?><p>
-                <a href="lista-de-reservaciones.php">Atrás</a>
             <?php
-                }
+            while ($reservacionEspecifica = $reservacionesDetalles->fetch_object()) {
+                $CheckIn = $reservacionEspecifica->checkInDateUser;
+                $hours = $reservacionEspecifica->hoursUser;
+                $checkOut = date('Y-m-d H:i:s', strtotime($CheckIn . ' + ' . $hours . ' hours'));
+            ?>
+                <div class="cont">
+                    <h1 class="Titulo--detalles-reservacion">Detalles de la Reservación</h1>
+                    <div class="datos">
+                        <div class="dato">
+                            <p class="negritas">Nombre:</p>
+                            <p><?php echo $reservacionEspecifica->nameUser; ?></p>
+                        </div>
+                        <hr class="hr--detalles-reservacion">
+                        <div class="dato">
+                            <p class="negritas">Email:</p>
+                            <p><?php echo $reservacionEspecifica->emailUser; ?></p>
+                        </div>
+                        <hr class="hr--detalles-reservacion">
+                        <div class="dato">
+                            <p class="negritas">Teléfono</p>
+                            <p><?php echo $reservacionEspecifica->phoneUser; ?></p>
+                        </div>
+                        <hr class="hr--detalles-reservacion">
+                        <div class="dato">
+                            <p class="negritas">Check In</p>
+                            <p><?php echo $reservacionEspecifica->checkInDateUser; ?></p>
+                        </div>
+                        <hr class="hr--detalles-reservacion">
+                        <div class="dato">
+                            <p class="negritas">Check Out</p>
+                            <p><?php echo $checkOut; ?></p>
+                        </div>
+                        <hr class="hr--detalles-reservacion">
+                        <div class="dato">
+                            <P class="negritas">Número de huespedes</P>
+                            <p><?php echo $reservacionEspecifica->guestsNumUser; ?></p>
+                        </div>
+                        <hr class="hr--detalles-reservacion">
+                        <div class="dato">
+                            <P class="negritas">Tipo de habitación</P>
+                            <p><?php echo $reservacionEspecifica->roomTypeUser; ?></p>
+                        </div>
+                        <hr class="hr--detalles-reservacion">
+                        <a class="boton-atras" href="lista-de-reservaciones.php">Atrás</a>
+                    </div>
+                </div>
+            <?php
+            }
             ?>
         </div>
     </div>
 </body>
+
 </html>
